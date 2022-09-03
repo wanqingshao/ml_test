7216bece257e7187a4e8bb1e35051076cf43fe07
wanqingshao <wanqingshao@gmail.com>
2022-09-03 15:55:13 -0400

    added perceptron and logistic regression

---
+++ scikit_learn_logistic_regression.py
@@ -0,0 +1,25 @@
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score, accuracy_score
+
+
+iris = datasets.load_iris()
+
+X = iris.data[:,[2,3]]
+y = iris.target
+
+X_norm = StandardScaler().fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_norm, y, test_size=0.3, random_state=1, stratify=y)
+
+lgr = LogisticRegression(C = 100, random_state=1, solver = "liblinear", multi_class="ovr")
+lgr.fit(X_train, y_train)
+y_hat = lgr.predict(X_test)
+
+acc_score = accuracy_score(y_test, y_hat)
+f1_score = f1_score(y_test, y_hat, average = "weighted")
+
+
+print("Accuracy score is {:.3f}, F1 score is {:.3f}".format(acc_score, f1_score)) ### much better than perceptron
---
+++ scikit_learn_perceptron.py
@@ -0,0 +1,26 @@
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score
+
+
+iris = datasets.load_iris()
+X = iris.data[:,[2,3]]
+y = iris.target
+
+X_normed = StandardScaler().fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_normed, y, random_state=1, stratify=y, test_size=0.3)
+
+percep = Perceptron(eta0 = 0.1, random_state=1)
+percep.fit(X_train, y_train)
+y_pred = percep.predict(X_test)
+
+acc_score = accuracy_score(y_test, y_pred)
+f1_score = f1_score(y_test, y_pred, average = "weighted")
+
+print("Accuracy score is {:.3f}, F1 score is {:.3f}".format(acc_score, f1_score))
+
+
+
+
